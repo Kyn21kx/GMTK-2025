475cac590d6bd000e6cab468954d6eff0fec1efc
Visualdavid <visualdavid1@gmail.com>
2025-07-31 21:24:18 -0500

    hud

---
+++ ui/hud.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@export var atk_basic: StyleBoxTexture
+@export var atk_basic_active: StyleBoxTexture
+@export var atk_magic: StyleBoxTexture
+@export var atk_magic_active: StyleBoxTexture
