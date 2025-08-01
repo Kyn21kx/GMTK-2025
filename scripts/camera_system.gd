9bbccc2c4bd2634c4e6992d802afeb9f3f2257ae
Kyn21kx <nefesmeralda@gmail.com>
2025-08-01 09:04:43 -0600

    Fix selectable

--- scripts/camera_system.gd
+++ scripts/camera_system.gd
@@ -5,25 +5,22 @@ extends Camera3D
 func _ready() -> void:
 	pass
 
-func _unhandled_input(event):
-	if event is InputEventMouse:
-		var from = project_ray_origin(event.position)
-		var to = from + project_ray_normal(event.position) * 1000
-		var space_state = get_world_3d().direct_space_state
-		var query = PhysicsRayQueryParameters3D.new()
-		query.from = from
-		query.to = to
-		# Optionally, set collision masks for specific layers to interact with
-		# query.set_collide_with_areas(true) 
-		query.set_collide_with_bodies(true)
+func _process(_delta: float) -> void:
+	var mouse_position: Vector2 = self.get_viewport().get_mouse_position()
+	var from = project_ray_origin(mouse_position)
+	var to = from + project_ray_normal(mouse_position) * 1000
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.new()
+	query.from = from
+	query.to = to
+	# Optionally, set collision masks for specific layers to interact with
+	# query.set_collide_with_areas(true) 
+	query.set_collide_with_bodies(true)
 
-		var result = space_state.intersect_ray(query)
+	var result = space_state.intersect_ray(query)
 
-		if result:
-			var clicked_object = result.collider as SelectableItem
-			if clicked_object == null:
-				return
-			clicked_object.hovering_over = true
-			print("Clicked object: ", clicked_object.name)
-		    # Add your selection logic here (e.g., highlight, select in a list)
-		    # Example: clicked_object.modulate = Color.RED # For a MeshInstance3D
+	if result:
+		var clicked_object = result.collider as SelectableItem
+		if clicked_object == null:
+			return
+		clicked_object.hovering_over = true
