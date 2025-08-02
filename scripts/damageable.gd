cf3028f1a47e6cc065699733ef5eb0ecaca03179
David Valencia <124006104+davidvalenciabattlerd@users.noreply.github.com>
2025-08-01 19:33:46 -0500

    enemy can fire

--- scripts/damageable.gd
+++ scripts/damageable.gd
@@ -1,16 +1,12 @@
 class_name Damageable
 extends Node
 
+signal died
+
 @export
 var _health: float = 100
 
 func damage(amount: float):
 	_health -= amount
 	if (_health <= 0):
-		die()
-
-func die():
-	# TODO: Get enemy type here and switch on it or smth
-	assert(get_parent() != null)
-	get_parent().queue_free();
-	pass
+		died.emit()
