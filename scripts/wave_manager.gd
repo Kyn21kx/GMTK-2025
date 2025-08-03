3bb50ca1710273915205d716d146aa6d401f6c19
Jonathan <soequelletindolu@gmail.com>
2025-08-03 12:53:06 -0600

    Add different spawn positions

--- scripts/wave_manager.gd
+++ scripts/wave_manager.gd
@@ -9,11 +9,14 @@ signal all_waves_completed
 @export var spawn_position: Node3D
 
 @export var waves: Array[Dictionary] = [
-	{"enemy_count": 5, "spawn_delay": 2.0, "wave_delay": 10.0},
-	{"enemy_count": 10, "spawn_delay": 1.5, "wave_delay": 15.0},
-	{"enemy_count": 15, "spawn_delay": 1.0, "wave_delay": 20.0},
-	{"enemy_count": 1, "spawn_delay": 1.0, "wave_delay": 5.0}
+	{"enemy_count": 5, "spawn_delay": 2.0, "wave_delay": 10.0, "spawn_spot": null},
+	{"enemy_count": 10, "spawn_delay": 1.5, "wave_delay": 15.0, "spawn_spot": null},
+	{"enemy_count": 15, "spawn_delay": 1.0, "wave_delay": 20.0, "spawn_spot": null},
+	{"enemy_count": 1, "spawn_delay": 1.0, "wave_delay": 5.0, "spawn_spot": null}
 ]
+
+@export var default_spawn_spot: Node3D
+@export var spawn_spots: Array[Node3D]
 
 @onready var spawn_timer = $SpawnTimer
 @onready var wave_timer = $WaveTimer
@@ -58,6 +61,9 @@ func _on_spawn_timer_timeout():
 		return
 
 	var current_wave_data = waves[current_wave_index]
+	var wave_spawn_spot: Node3D = default_spawn_spot
+	if current_wave_index + 1 <= spawn_spots.size():
+		wave_spawn_spot = spawn_spots[current_wave_index]
 
 	if enemies_spawned_in_wave >= current_wave_data["enemy_count"]:
 		is_in_wave = false
@@ -67,7 +73,7 @@ func _on_spawn_timer_timeout():
 		wave_timer.start()
 		return
 
-	spawn_enemy()
+	spawn_enemy(wave_spawn_spot.global_position)
 	enemies_spawned_in_wave += 1
 
 
@@ -75,12 +81,11 @@ func _on_wave_timer_timeout():
 	start_next_wave()
 
 
-func spawn_enemy():
+func spawn_enemy(spawn_pos: Vector3):
 	if not enemy_scene:
 		printerr("ERROR: La escena del enemigo no está asignada en el WaveManager.")
 		return
 
 	var enemy_instance: CharacterBody3D = enemy_scene.instantiate() as CharacterBody3D
-	var enemy_spawn_pos: Vector3 = spawn_position.global_position
-	enemy_instance.position = enemy_spawn_pos
+	enemy_instance.position = spawn_pos
 	get_parent().add_child(enemy_instance)
